0424687a7a01f41b70abb1429352fa1bd823a5ef
andy reyes <11710reyes@gmail.com>
2018-09-18 18:01:28 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,5 +4,5 @@ function  multiply(a,b){  return(a*b)}
 function  divide(a,b){  return(a/b)}
 function inc(n){  return(n+=1)}
 function dec(n){  return(n-=1)}
-function makeInt(n) {return(parseInt('2', 10))} 
+function makeInt(n) {return(parseInt('', 10))} 
 function preserveDecimal(n){  return(parseFloat(n))}
